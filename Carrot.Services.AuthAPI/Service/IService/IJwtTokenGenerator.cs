0a658f6078afde8d417803e4607beb7fdecda147
Uchenna Nwaigwe <hermessuffice@gmail.com>
2025-06-25 10:34:38 +0100

    End of Consuming AuthAPI - 5

---
+++ Carrot.Services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Carrot.Services.AuthAPI.Models;
+
+namespace Carrot.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+
+    }
+}
